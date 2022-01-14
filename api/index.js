e589d79a38a4c8acb30e505d6ac3697b87dcdd1e
Tim Jackson <timjacksonm@gmail.com>
2022-01-14 09:38:16 -0600

    removed rle-decoder as I will be using it on my front end instead. Decided to store the rle string as is to save space on the database

--- api/index.js
+++ api/index.js
@@ -3,6 +3,31 @@ const router = express.Router();
 const WikiTemplate = require('../models/template');
 const template_controller = require('../controllers/officialTemplateController');
 
+//get random pattern
+router.get('/:collection/pattern', async (req, res) => {
+  const { collection } = req.params;
+
+  switch (collection) {
+    case 'wikitemplates':
+      const response = await WikiTemplate.findOne();
+      res.status(200).json(response);
+      break;
+    case 'customtemplates':
+      res.send('test');
+      break;
+
+    default:
+      res.status(400).json({ message: 'collection not found.' });
+      break;
+  }
+});
+
+//get random patterns options: {howmany: num}
+router.get('/:collection/patterns', async (req, res) => {});
+
+//get pattern by :id options {whattoinclude: 0 || 1}
+router.get('/:collection/patterns/:id', async (req, res) => {});
+
 //get all pattern names options: {includedescriptions: 0 || 1}
 router.get('/:collection/titles', async (req, res) => {});
 
@@ -11,14 +36,5 @@ router.get('/:collection/:title', async (req, res) => {});
 
 //find pattern by author
 router.get('/:collection/:author', async (req, res) => {});
-
-//get random patterns options: {howmany: num}
-router.get('/:collection/patterns', async (req, res) => {});
-
-//get random pattern
-router.get('/:collection/pattern', async (req, res) => {});
-
-//get pattern by :id options {whattoinclude: 0 || 1}
-router.get('/:collection/pattern/:id', async (req, res) => {});
 
 module.exports = router;
