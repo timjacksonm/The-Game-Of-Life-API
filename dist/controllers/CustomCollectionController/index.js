359bd16573d4b9af05d925bd986ca251e43e043f
Tim Jackson <timjacksonm@gmail.com>
2022-10-12 18:36:09 -0500

    added dist to gitignore

--- dist/controllers/CustomCollectionController/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const customCollectionController_1 = __importDefault(require("./customCollectionController"));
-exports.default = customCollectionController_1.default;
