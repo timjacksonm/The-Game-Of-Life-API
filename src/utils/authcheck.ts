7a0cd54cf91e84b54039abb55f8e075a5e7b3730
Timothy <timjacksonm@gmail.com>
2023-07-13 22:12:56 -0500

    disabled

--- src/utils/authcheck.ts
+++ src/utils/authcheck.ts
@@ -1,18 +1,19 @@
-import { NextFunction, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
 dotenv.config();
 
 export const auth = (req: Request, res: Response, next: NextFunction) => {
-  const { RAPID_API_KEY } = process.env;
-  if (!req.header('X-RapidAPI-Proxy-Secret')) {
-    return res.status(401).json({
-      message: 'Invalid request. Must include X-RapidAPI-Proxy-Secret Header',
-    });
-  }
+  // disabled since no longer hosted on rapidapi
+  // const { RAPID_API_KEY } = process.env;
+  // if (!req.header('X-RapidAPI-Proxy-Secret')) {
+  //   return res.status(401).json({
+  //     message: 'Invalid request. Must include X-RapidAPI-Proxy-Secret Header',
+  //   });
+  // }
 
-  if (req.header('X-RapidAPI-Proxy-Secret') !== RAPID_API_KEY) {
-    return res.status(401).json({ message: 'Invalid API Key' });
-  }
+  // if (req.header('X-RapidAPI-Proxy-Secret') !== RAPID_API_KEY) {
+  //   return res.status(401).json({ message: 'Invalid API Key' });
+  // }
 
   next();
 };
