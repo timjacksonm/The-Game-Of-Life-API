2abdc38c6a21ca4dd6814cf9856a1e99879bd4bf
Timothy <timjacksonm@gmail.com>
2023-07-19 22:08:59 -0500

    added rate limiter to index

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import compression from "compression";
 import cors from "cors";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 import home from "./controllers/home";
 import wikiRoutes from "./controllers/WikiCollectionController";
 import customRoutes from "./controllers/CustomCollectionController";
@@ -49,6 +50,16 @@ db.on("connected", () => info("MongoDB connection established successfully"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
+// app.set('trust proxy', 1)
 
 app.get("/", home);
 app.use("/", authcheck);
